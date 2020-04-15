f19b1a508fa26991c0ba3a0e0150579f621f568f
vietokok <khacviet201299@gmail.com>
2020-04-15 10:40:48 +0700

    Add click to change display

--- changetext/src/App.js
+++ changetext/src/App.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import ChangeText from "./components/ChangeText";
+import DisplayState from "./components/DisplayState";
 
-function App() {
-	return (
-		<div className="App">
-			<ChangeText />
-		</div>
-	);
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			display: "block",
+		};
+
+		this.onClickButton = this.onClickButton.bind(this);
+	}
+
+	onClickButton() {
+		this.setState({
+			display: this.state.display == "block" ? "none" : "block",
+		});
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<ChangeText />
+				<DisplayState
+					display={this.state.display}
+					onClick={this.onClickButton}
+				/>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ changetext/src/components/DisplayState.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./DisplayState.css";
+
+class DisplayState extends Component {
+	render() {
+		const { display, onClick } = this.props;
+		return (
+			<div className="DisplayState">
+				<button onClick={onClick}>Click me</button>
+				<div className={display}>You are amazing</div>
+			</div>
+		);
+	}
+}
+
+export default DisplayState;
